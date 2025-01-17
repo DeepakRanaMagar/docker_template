ed3f5e3baf36e56c3018a285d9a04d862c252a39
DeepakRanaMagar <dpk.me.123@gmail.com>
2025-01-17 15:39:54 +0545

    feat: devcontainer setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,17 +24,17 @@ done
 # Check the value of --dev and perform actions
 if [[ "$DEV" == "true" ]]; then
     echo "Running in development mode..."
-    python manage.py makemigrations --noinput
-    python manage.py migrate
-    python manage.py runserver 0.0.0.0:8000
+    python src/manage.py makemigrations --noinput
+    python src/manage.py migrate
+    python src/manage.py runserver 0.0.0.0:8000
 elif [[ "$DEV" == "false" ]]; then
     echo "Running in production mode..."
-    python manage.py makemigrations --noinput
-    python manage.py migrate
-    python manage.py collectstatic --no-input
+    python src/manage.py makemigrations --noinput
+    python src/manage.py migrate
+    python src/manage.py collectstatic --no-input
     sudo chown -R www-data:www-data /app/staticfiles
     sudo chmod -R 755 /app/staticfiles
-    gunicorn --bind 0.0.0.0:8000 backend.wsgi
+    gunicorn --bind 0.0.0.0:8000 core.wsgi
 else
     echo "Error: Please specify --dev=true or --dev=false"
     exit 1
